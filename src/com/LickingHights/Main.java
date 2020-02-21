e85306f7ace930b9faa38bc5c9a9e323935a5d6b
puskar <puskar.subedi.2021@student.lhschools.org>
2020-02-21 14:07:32 -0500

    intellij commit

---
+++ src/com/LickingHights/Main.java
@@ -0,0 +1,19 @@
+package com.LickingHights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("MY name is ");
+        System.out.println("Puskar");
+
+
+
+        System.out.println("I went to vist my famlly.");
+        System.out.println("I played soccer.");
+        System.out.println("And I was on the sleeping");
+
+}
+
+
+
